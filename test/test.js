eedea19e85177ce2c80e38c878ca43ff96e343dc
Larry Wu <lw7360@gmail.com>
2015-09-17 19:48:27 +0000

    More coverage tests

--- test/test.js
+++ test/test.js
@@ -23,4 +23,14 @@ describe('#ecckey', function() {
 
     expect(decrypted).to.equal(text);
   });
+
+  it('should encrypt and decrypt random text with a strange # of bits', function() {
+    var pass = chance.string();
+    var keys = ecckey.generate(1337, pass);
+    var text = chance.paragraph();
+    var encrypted = ecckey.encrypt(pass, keys, text);
+    var decrypted = ecckey.decrypt(pass, keys, encrypted);
+
+    expect(decrypted).to.equal(text);
+  });
 });
