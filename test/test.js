e73347f9084d9bd52b3a31381aabdddf9145bf54
Larry Wu <lw7360@gmail.com>
2015-09-17 10:02:55 -0700

    Tests

---
+++ test/test.js
@@ -0,0 +1,26 @@
+var Chance = require('chance');
+var chance = new Chance();
+var expect = require('chai').expect;
+var ecckey = require('../index');
+
+describe('#ecckey', function() {
+  it('should encrypt and decrypt', function() {
+    var pass = 'super secure and secret passphrase';
+    var keys = ecckey.generate(384, pass);
+    var text = 'interesting text';
+    var encrypted = ecckey.encrypt(pass, keys, text);
+    var decrypted = ecckey.decrypt(pass, keys, encrypted);
+
+    expect(decrypted).to.equal(text);
+  });
+
+  it('should encrypt and decrypt random text', function() {
+    var pass = chance.string();
+    var keys = ecckey.generate(384, pass);
+    var text = chance.paragraph();
+    var encrypted = ecckey.encrypt(pass, keys, text);
+    var decrypted = ecckey.decrypt(pass, keys, encrypted);
+
+    expect(decrypted).to.equal(text);
+  });
+});
